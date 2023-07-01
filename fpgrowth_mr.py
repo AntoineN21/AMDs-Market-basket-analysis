10c718b51ae9038befdc9d8f660fb68258d3063e
Antoine-nasra <121320398+AntoineN21@users.noreply.github.com>
2023-07-01 19:33:42 +0200

    Update fpgrowth_mr.py

--- fpgrowth_mr.py
+++ fpgrowth_mr.py
@@ -98,6 +98,7 @@ def mine_frequent_itemsets(frequent_items, item_counts, min_support, conditional
     for item in frequent_items:
         support = item_counts.get(prefix + (item,), 0)
         print("supp")
+        print(item_counts)
         print(support)
         print(min_support)
         if support >= min_support:
